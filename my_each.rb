b742a947c02d4a64bc3b031e424971771c8e275e
Gian Libunao <gianlibunao@gmail.com>
2019-04-29 01:47:53 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,9 @@
 def my_each(array) # put argument(s) here
   # code here
-  array.each { |x| puts x}
+  #array.each { |x| puts x}
+  counter = 0
+  while counter<array.length
+  yield array[counter]
+  counter+=1
+end
 end
